2f7c9366a6730b9c2a563d0aa49923238b065bc4
Ashley Hart <ahart425@gmail.com>
2022-05-21 08:52:52 -0600

    editing homepage text and width

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import './Scss/style.scss';
-import Navigation from './Navbar';
+import React from "react";
+import "./Scss/style.scss";
+import Navigation from "./Navbar";
 // import Aframe from './aframe';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HomePageWelcome = () => {
-    return (
-        <div id='homepage-container' className='section-container'>
-           <h1 className='sectionTitle'>Ashley Hart</h1>
-           <div></div>
-           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-    )
-}
+  return (
+    <div id="homepage-container" className="section-container">
+      <h1 className="sectionTitle">Ashley Hart</h1>
+      <div></div>
+      <div class='home-page-text'>
+      <span>Welcome!</span>
+      <br/>
+      <br/>
+      <span>
+          I am a Front-End Web Developer who enjoyes diving into the small details of design and styling. I am constantly developing my skills to stay current with the newest technologies, and enjoy implementing these new trends and concepts into my work.
+      </span>
+      <br/>
+      <br/>
+      <span>
+        While not online, I enjoy running trails in the high country, friendly
+        competition at the CrossFit gym, and camping under the stars in the
+        summer.
+      </span>
+      </div>
+    </div>
+  );
+};
 
 export default HomePageWelcome;
