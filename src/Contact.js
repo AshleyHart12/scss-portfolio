2b999f22c5571bcefbc6eb742a9a590979983149
Ashley Hart <ahart425@gmail.com>
2022-06-16 08:12:59 -0600

    went back a few commits to solve deployment issues, email functionality added back in, still works

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Scss/style.scss";
 import Form from "react-bootstrap/Form";
 import { Container, Button, Row } from "react-bootstrap";
 import { Element } from 'react-scroll';
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
+
+  const form = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_jwc0ike",
+        "template_lfihv3a",
+        form.current,
+        "fF1jDB4LOgD5Dxqoo"
+      )
+      .then(
+        (res) => {
+          console.log(res.text);
+        },
+        (err) => {
+          console.log(err.text);
+        }
+      );
+      e.target.reset();
+  };
   return (
-    <Element id='contact' name='contact'>
-    <div className="section-container" id='contact-container'>
+    <Element id="contact" name="contact">
+    <div className="section-container" id="contact-container">
       <Container id="contact-form-container">
         <Row>
           <h1 className="sectionTitle">Contact</h1>
@@ -20,7 +43,7 @@ const ContactForm = () => {
           >
             <img
               src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2FlinkedIn%2FlinkedIn_PNG24.png&f=1&nofb=1"
-              style={{ width: "60px", height: "60px", margin: '10px' }}
+              style={{ width: "60px", height: "60px", margin: "10px" }}
               alt="link to linkedin profile"
             />
           </a>
@@ -31,35 +54,29 @@ const ContactForm = () => {
           >
             <img
               src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fdownload%2Fwhite%2Fgithub-10-256.png&f=1&nofb=1"
-              style={{ width: "60px", height: "60px", margin: '10px' }}
+              style={{ width: "60px", height: "60px", margin: "10px" }}
               alt="link to gituhb profile"
             />
           </a>
         </div>
-        <Form>
-          {/* NAME */}
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Name" />
-          </Form.Group>
-          {/* EMAIL */}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          {/* MESSAGE */}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Message</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter Message"
-              style={{ height: "100px" }}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+
+        <form onSubmit={onSubmit} id="contact-form" ref={form}>
+          <Row>
+            <label class='form-label'>Name</label>
+            <input type="text" name="from_name" />
+          </Row>
+          <Row>
+            <label class='form-label'>Email</label>
+            <input type="email" name="from_email" />
+          </Row>
+          <Row>
+            <label class='form-label'>Message</label>
+            <textarea name="message" />
+          </Row>
+          <Row>
+           <Button type="submit" value="message" id='form-submit' class='btn btn-primary'>Submit</Button>
+          </Row>
+        </form>
       </Container>
     </div>
     </Element>
