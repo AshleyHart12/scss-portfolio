af736595223b8c4b57678b7b804e077a37ead86f
Ashley Hart <ahart425@gmail.com>
2022-08-04 09:49:53 -0600

    added beating heart to home page, added color changing tex to homepage

--- src/Home.js
+++ src/Home.js
@@ -19,21 +19,21 @@ const HomePageWelcome = () => {
           loop
         /> */}
       <div>
-         <h1 class='sectionTitle'>Ashley Hart</h1>
+         <h1 class='sectionTitle'><span class='title-word1 title-word'>Ashley</span><span class='title-word2 title-word'> Hart</span></h1>
       </div>
       <div class='home-page-text'>
       <span>Welcome!</span>
       <br/>
       <br/>
       <span>
-          I am a Front-End Web Developer who enjoyes diving into the small details of design and styling. I am constantly developing my skills to stay current with the newest technologies, and enjoy implementing these new trends and concepts into my work.
+          I am a Front-End Web Developer who enjoyes diving into the small details of design and styling. I am constantly developing my skills to stay current with the newest technologies, and enjoy implementing these new trends and concepts into my work. 
       </span>
       <br/>
       <br/>
       <span>
-        While not online, I enjoy running trails in the high country, friendly
+        While not online, I <span><img src='https://denisechandler.com/wp-content/themes/portfolio_oct2021/images/heart2.png' alt='heart that throbs with css animations' id='homePageHeart' /></span> running trails in the high country, friendly
         competition at the CrossFit gym, and camping under the stars in the
-        summer.
+        summer
       </span>
       </div>
     </div>
